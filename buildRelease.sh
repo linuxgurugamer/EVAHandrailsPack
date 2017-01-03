1f0f6e85e1a7851ed60d12af56b349ffa8dabbe3
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-01-03 07:58:00 -0500

    repackaged in more standard layout

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+v="NEBULA/EVAHandrailsPackContinued.version"
+major=`grep -m 1 MAJOR $v | cut -f2 -d':' | tr -d , | tr -d '[:space:]'`
+minor=`grep -m 1 MINOR $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
+patch=`grep -m 1 PATCH $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
+build=`grep -m 1 BUILD $v | cut -f2 -d':' | tr -d ,| tr -d '[:space:]'`
+version="${major}.${minor}.${patch}"
+if [ "$build" != "0" ]; then
+		version="${version}.${build}"
+fi
 
-cat NEBULA/EVAHandrailsPackContinued.version
+echo $version
 
-echo "Enter version:"
-read version
+mkdir GameData
+cp ../ModuleManager*.dll GameData
+cp -a NEBULA GameData/NEBULA
 
-zip -9r EVAHandrailsPack-${version}.zip NEBULA
+releasedir="."
+fname="${releasedir}/EVAHandrailsPack-${version}.zip"
+zip -9r $fname GameData
+rm -fr GameData/NEBULA GameData/*.dll
